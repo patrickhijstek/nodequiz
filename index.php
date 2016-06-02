bd1aa1116fdc359b25a5e73dd4c8617943f91fcf
Roderik Uenk <r.uenk@mastersinlean.com>
2016-06-02 20:52:00 +0200

    start roderik
    Start.html aangemaakt

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: patri
- * Date: 2-6-2016
- * Time: 19:09
- */ 
---
+++ js/app.js
@@ -0,0 +1,5 @@
+$( document ).ready(function() {
+	console.log("test");
+	$("#data").html("<img id='logo' src='images/nodejs-logo.png'>"
+		);
+});
