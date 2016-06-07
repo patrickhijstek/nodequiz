a2352999a615ac9d7ebf7c09ac7730db5a7b0a39
Roderik Uenk <r.uenk@mastersinlean.com>
2016-06-07 17:17:42 +0200

    startscherm

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-	console.log("test");
-	$("#data").html("<img id='logo' src='images/nodejs-logo.png'>"
+	$("#data").html("<div><img id='logo' src='images/nodejs-logo.png'>\n" + 
+	"<p id='logotekst'>Quiz</p></div>" 
 		);
 });
