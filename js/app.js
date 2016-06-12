8fd7dedc9451373582b6252c096346d32779d445
Roderik Uenk <r.uenk@mastersinlean.com>
2016-06-12 17:26:51 +0200

    losse pagina's , uitslag toegeveogd

--- js/app.js
+++ js/app.js
@@ -7,20 +7,76 @@ $( document ).ready(function() {
 	});
 
 
-	function loginPage(){
 
-	$("#data").load("../login.html #nav1div");
-	//$("#data").show();
-}
-	
+  $('.antwoorden').on('click', 'div', function(event) {
+  		event.preventDefault();
+  		$('.antwoorden > div').removeClass('selected');
+  		
+  		$(this).addClass('selected');
+  	});
+
+  $("#setPlayer").on("click", "input:submit", function(){
+    alert("form has been submitted.");
+    return false;
+});
+
 
 	$(".meter > span").each(function() {
   $(this)
     .data("origWidth", $(this).width())
     .width(0)
     .animate({
-      width: $(this).data("origWidth") // or + "%" if fluid
-    }, 10000);
-});
+    	width: $(this).data("origWidth") // or + "%" if fluid
+    	}, 10000);
+	});
 });
 
+function loginPage(){
+html = $.get( "login.html", function() {
+  $('main').html(html.responseText);
+$('main').show();
+})
+  .fail(function() {
+    alert( "error" );
+  }); 
+}
+
+
+function lobbyPage(){
+ html = $.get( "lobby.html", function() {
+  console.log(html.responseText);
+  $('main').html(html.responseText);
+
+})
+  .fail(function() {
+    alert( "error" );
+  }); 
+}
+
+function NieuwSpel(){
+ html = $.get( "spel.html", function() {
+   $('main').html(html.responseText);
+})
+  .fail(function() {
+    alert( "error" );
+  }); 
+}
+
+function JoinSpel(id){
+ html = $.get( "spel.html", function() {
+  alert("spel id = " + id);
+  $('main').html(html.responseText);
+
+})
+  .fail(function() {
+    alert( "error" );
+  }); 
+}
+function uitslagPage(){
+  html = $.get( "uitslag.html", function() {
+  $('main').html(html.responseText);
+  })
+    .fail(function() {
+      alert( "error" );
+    }); 
+}
